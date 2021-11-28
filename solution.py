9f6492513d5fb12f769c9c6838533b4a53a034c5
austinbudhram99 <90538815+austinbudhram99@users.noreply.github.com>
2021-11-27 23:59:15 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -8,6 +8,10 @@ import binascii
 from types import CodeType
 from typing import Sequence
 # Should use stdev
+packet_min = 0
+packet_avg = 0
+packet_max = 0
+stdev_var = 0
 
 ICMP_ECHO_REQUEST = 8
 
@@ -114,13 +118,12 @@ def ping(host, timeout=1):
     print("Pinging " + dest + " using Python:")
     print("")
     # Calculate vars values and return them
-    #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
+    vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
         print(delay)
         time.sleep(1)  # one second
-
     return vars
 
 if __name__ == '__main__':
